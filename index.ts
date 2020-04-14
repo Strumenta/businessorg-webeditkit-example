bc3bf217846d8fcac1ba6087393b1e7f9be0485d
Federico Tomassetti <federico@tomassetti.me>
2020-04-14 17:14:13 +0200

    show also process

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
+import {ModelNode} from "webeditkit/dist/datamodel";
+
+var css = require('./css/style.css');
+
 import {
     editableCell,
     emptyRow,
-    fixedCell, horizontalCollectionCell, referenceCell,
+    fixedCell, horizontalCollectionCell, horizontalGroupCell, referenceCell,
     registerRenderer,
     row, tabCell,
     verticalCollectionCell,
     verticalGroupCell
 } from "webeditkit";
-import {ModelNode} from "webeditkit/dist/datamodel";
-
-var css = require('./css/style.css');
 
 const webeditkit = require("webeditkit");
 
@@ -42,5 +43,32 @@ $('document').ready(function(){
         return referenceCell(node, 'role');
     });
 
-    webeditkit.addModel("localhost:2904", "com.strumenta.businessorg.sandbox.acmeinc", "5270253970127314084", "businessorg");
+    registerRenderer('com.strumenta.businessorg.Process', (node) => {
+        return verticalGroupCell(
+            row(fixedCell(node, 'process', ['title']),
+                editableCell(node, 'name', ['title']),
+                fixedCell(node, 'for', ['title']),
+                referenceCell(node, 'organization', ['title'])),
+            emptyRow(),
+            verticalCollectionCell(node, 'steps', true),
+            // row(fixedCell(node, 'Roles:')),
+            // row(tabCell(), verticalCollectionCell(node, 'roles')),
+            // emptyRow(),
+            // row(fixedCell(node, 'Persons:')),
+            // row(tabCell(), verticalCollectionCell(node, 'persons')),
+        );
+    });
+
+    registerRenderer('com.strumenta.businessorg.ProcessStep', (node) => {
+        return horizontalGroupCell(
+            tabCell(),
+            fixedCell(node, (node.index() + 1).toString()),
+            editableCell(node, 'name'),
+            fixedCell(node, 'executed by'),
+            referenceCell(node, 'executor')
+        );
+    });
+
+    webeditkit.addModel("localhost:2904", "com.strumenta.businessorg.sandbox.acmeinc", "5270253970127314084", "organization");
+    webeditkit.addModel("localhost:2904", "com.strumenta.businessorg.sandbox.acmeinc", "5270253970127368335", "process");
 });
--- updatewek.sh
+++ updatewek.sh
@@ -1,1 +1,1 @@
-npm install ../webeditkit/webeditkit-1.0.4.tgz
+npm install ../webeditkit/webeditkit-1.0.5.tgz
